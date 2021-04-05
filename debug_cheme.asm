ec8a717eed9f49095e9b86941f5b54c5139303c7
Alexander Dolgy <dolgiy@ligarobotov.ru>
2021-04-05 14:50:22 +0700

    refactoring

--- debug_cheme.asm
+++ debug_cheme.asm
@@ -1,6 +1,9 @@
 asect 0x00
+jsr A
+halt
+A:
 ldi r0, 0xff
 ldi r1, 1
 st r0, r1
-halt
+rts
 end
--- test.asm
+++ test.asm
@@ -1,42 +1,9 @@
-#address of data and adress of id
-macro write_to_field /2
-	push r0
-	push r1
-	push r2
-	push r3
-	#r0 - mask
-	ldi r0, 0b00010000
-	#r1 - adress of data to write
-	#ldi r1, $1
-	move $0, r1
-	ld r1, r1
-	#r2 - adress of id 
-	#ldi r2, $2
-	move $1, r1
-	ld r2, r2
-	or r0, r2
-	
-	ldi r3, 0xfe
-	st r3, r1
-	ldi r3, 0xfc
-	st r3, r2
-	ldi r2, 0
-	st r3, r2
-	pop r3
-	pop r2
-	pop r1
-	pop r0
-mend
 
 asect	0x00
 	# WRITE YOUR CODE HERE
+	br MAIN
 	
-	
-	ldi r0, 0x0E
-	ld r0, r0
-	if
-	tst r0
-	is z
+	INIT_MEMORY:
 	#0b00000000
       #^^^^^^^^
       #||||||||
@@ -52,7 +19,6 @@ asect	0x00
 		ldi r0, 0x0f
 		ldi r1, 0b00100000
 		st r0, r1  
-		addsp 0xF0
 		ldi r1, 13
 		ldi r2, 0
 		ldi r3, 4
@@ -97,8 +63,17 @@ asect	0x00
 		ldi r0, 0x0E
 		ldi r1, 1
 		st r0, r1
-	fi
-	#write_to_field 0x00, 0x00
+		
+		ldi r0, 0x0f
+		ldi r1, 0b00000000
+		st r0, r1
+	rts
+	# END INIT MEMORY
+	#BEGIN OF DISPLAY DRAW
+	WRITE_TO_FIELD:
+	ldi r0, 0x0f
+		ldi r1, 0b00000010
+		st r0, r1  
 	ldi r0, 0x0D
 	while 
 	tst r0 
@@ -120,6 +95,25 @@ asect	0x00
 	pop r0
 	dec r0
 	wend
+	ldi r0, 0x0f
+		ldi r1, 0b00000000
+		st r0, r1
+	rts
+	#BEGIN MAIN
+	MAIN:
+	#write_to_field 0x00, 0x00
+	#check that initializing is needed
+	ldi r0, 0x0E
+	ld r0, r0
+	if
+	tst r0
+	is z
+	addsp 0xF0
+	jsr INIT_MEMORY
+	fi
+	
+	
+	jsr WRITE_TO_FIELD
 	halt
 	#switch to 2 bank
 asect 0xF7
