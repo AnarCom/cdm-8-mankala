fea2d9e11cbc84c82131b08c6f333f8cd684e5fe
Alexander Dolgy <dolgiy@ligarobotov.ru>
2021-05-10 22:22:15 +0700

    added ai by rand char, may be it's time to create my own rand char generator?)

--- bank_2_ai.asm
+++ bank_2_ai.asm
@@ -1,5 +1,42 @@
 asect 0x00
+START:
+#rand char in r1
+ldi r1, 0xFA
+ld r1, r1
 
+ldi r0, 0b00000111
+and r0, r1
 
+ldi r0, 6
 
+if
+	sub r1, r0
+is pl
+	br START
+fi
+
+ldi r0, 0x07
+add r0, r1
+
+ld r1, r2
+if
+tst r2
+is z
+	br START
+fi
+
+ldi r0, 0x10
+add r0, r1
+
+ld r1, r1
+
+ldi r0, 0x1F
+st r0, r1
+ldi r0, 1
+br CHANGE
+asect 0xF7
+	CHANGE:
+	ldi r1, 0xff	
+	st r1, r0
+	br 0
 end.
