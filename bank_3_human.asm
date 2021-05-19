0b090300041ac82b12695c1bb3b447180a37b374
Alexander Dolgiy <alex20021505@gmail.com>
2021-05-19 09:17:10 +0700

    added test for xero at human hole
    Если в лунке у игрока 0, то происходит повторный ввод кода _ не протестировано

--- bank_3_human.asm
+++ bank_3_human.asm
@@ -5,6 +5,8 @@ asect	0x00
 	ldi r1, 0b00001000
 	st r0, r1
 	
+	START:
+	
 	ldi r0, 0xF9
 	ld r0, r1
 	while 
@@ -12,6 +14,17 @@ asect	0x00
 	stays z
 	ld r0, r1
 	wend
+	
+	move r1, r2
+	ld r2
+	if
+		tst r2
+	is z
+		ldi r0, 0x0f
+		ldi r1, 0b01000000
+		st r0, r1 
+		br START
+	fi
 	ldi r0, 0x1F
 	st r0, r1
 	
@@ -31,12 +44,13 @@ asect	0x00
   #  > Ready
 	ldi r0, 0x0f
 	ldi r1, 0b00000000
-	 
 	ldi r0, 4
-	br 0xF7
+	
+	br SWITCH_BANK
 	#перед попаданием в функцию в r0 должен быть адрес банки для перехода
 	#switch to 2 bank
 	asect 0xF7
+	SWITCH_BANK:
 	ldi r1, 0xff	
 	st r1, r0
 	br 0
